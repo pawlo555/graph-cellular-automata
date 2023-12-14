3a55e805cd672e3d48a2d52030247b2b8ee38288
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2023-12-14 11:04:50 +0100

    Refactor code and add arguments parser

--- graph_colouring/gradient_coloring.py
+++ graph_colouring/gradient_coloring.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+
 import matplotlib.pyplot as plt
 import networkx as nx
 import torch
@@ -11,9 +13,9 @@ def continuous_loss(embeddings, graph):
     return sum(torch.dot(embeddings[i], embeddings[j]) for i, j in graph.edges)
 
 
-def graph_coloring(graph, k):
+def graph_coloring(graph, k, max_iter, lr):
     embeddings = torch.rand((len(graph), k), requires_grad=True)
-    optimizer = torch.optim.SGD([embeddings], lr=0.1)
+    optimizer = torch.optim.SGD([embeddings], lr=lr)
     softmax = torch.nn.Softmax(dim=1)
 
     discrete_loss_history = []
@@ -37,7 +39,7 @@ def graph_coloring(graph, k):
         if step % 100 == 0:
             print(f"Step {step} - discrete loss: {d_loss}, continuous loss: {c_loss}")
 
-        if d_loss == 0 or step >= 1000:
+        if d_loss == 0 or step >= max_iter:
             break
 
         step += 1
@@ -45,34 +47,59 @@ def graph_coloring(graph, k):
     return embeddings.argmax(dim=1).numpy(), discrete_loss_history, continuous_loss_history
 
 
+def train_with_restarts(graph, k, max_iter, lr, restarts):
+    best_colors = None
+    best_loss = float('inf')
+
+    for _ in range(restarts):
+        network_colors, d_loss_history, c_loss_history = graph_coloring(graph, k, max_iter, lr)
+
+        if d_loss_history[-1] < best_loss:
+            best_loss = d_loss_history[-1]
+            best_colors = network_colors
+
+        if d_loss_history[-1] == 0:
+            break
+
+    return best_colors, d_loss_history, c_loss_history
+
+
 if __name__ == '__main__':
-    G = nx.gnm_random_graph(30, 80, seed=42)
+    args = ArgumentParser()
+    args.add_argument('--n', type=int, default=30, help='Number of nodes')
+    args.add_argument('--m', type=int, default=80, help='Number of edges')
+    args.add_argument('--seed', type=int, default=42, help='Random seed for graph generation')
+    args.add_argument('--restarts', type=int, default=10, help='Number of restarts')
+    args.add_argument('--max_iter', type=int, default=1000, help='Maximum number of iterations')
+    args.add_argument('--lr', type=float, default=0.1, help='Learning rate')
+    args = args.parse_args()
+
+    G = nx.gnm_random_graph(args.n, args.m, args.seed)
     layout = nx.spring_layout(G)
 
     greedy_colors_dict = nx.greedy_color(G)
-    greedy_colors = []
+    greedy_colors = [greedy_colors_dict[key] for key in sorted(greedy_colors_dict.keys())]
 
-    for key in sorted(greedy_colors_dict.keys()):
-        greedy_colors.append(greedy_colors_dict[key])
+    best_colors, d_loss_history, c_loss_history = train_with_restarts(
+        graph=G,
+        k=max(greedy_colors) + 1,
+        max_iter=args.max_iter,
+        lr=args.lr,
+        restarts=args.restarts
+    )
 
-    for _ in range(10):
-        network_colors, discrete_loss_history, continuous_loss_history = graph_coloring(G, max(greedy_colors) + 1)
-
-        if discrete_loss_history[-1] == 0:
-            break
-
-    plt.plot(discrete_loss_history, label='Discrete loss')
-    plt.plot(continuous_loss_history, label='Continuous loss')
+    plt.plot(d_loss_history, label='Discrete loss')
+    plt.plot(c_loss_history, label='Continuous loss')
     plt.xscale('log')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
     plt.show()
 
-    plt.title("Proper coloring")
+    plt.title("Greedy coloring")
     nx.draw(G, pos=layout, node_color=greedy_colors, node_size=100)
     plt.show()
 
     plt.title("Gradient coloring")
-    nx.draw(G, pos=layout, node_color=network_colors, node_size=100)
+    nx.draw(G, pos=layout, node_color=best_colors, node_size=100)
     plt.show()
